3af068459b3d733dfc7eb9b8f66190accce0fdf7
Hope Njoroge <hnjoroge64@gmail.com>
2024-05-31 22:27:50 +0000

    add: add NavBar component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from 'react-bootstrap/Button';
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
-     <h1>Welcome to Managey!</h1>
+     <NavBar/>
     </div>
   );
 }
