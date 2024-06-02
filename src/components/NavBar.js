915e256da9cdf75a0282fb0a4a79c1ee24e2be09
Hope Njoroge <hnjoroge64@gmail.com>
2024-06-02 22:10:48 +0000

    add: add logo, navigation icons and favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,18 +2,27 @@ import React from "react";
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import managey from '../assets/managey.png'
 
 const NavBar = () => {
   return (
-    <Navbar expand="lg" className="bg-body-tertiary" fixed="top">
+    <Navbar expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">Managey</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={managey} alt="logo" height="50" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#tasks">Tasks</Nav.Link>
-            <Nav.Link href="#logout">Logout</Nav.Link>
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fa-solid fa-house">Home</i>
+            </Nav.Link>
+            <Nav.Link >
+              <i className="fa-solid fa-right-to-bracket">Login</i>
+            </Nav.Link>
+            <Nav.Link >
+              <i className="fa-solid fa-user-plus">Sign up</i>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
