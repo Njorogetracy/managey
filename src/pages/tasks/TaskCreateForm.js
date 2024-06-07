a38ba8c54b851bdeddd1c536058ba3ad680baa6a
Hope Njoroge <hnjoroge64@gmail.com>
2024-06-07 14:02:43 +0000

    add: create handleChange function so users can fill the input fields

--- src/pages/tasks/TaskCreateForm.js
+++ src/pages/tasks/TaskCreateForm.js
@@ -1,41 +1,49 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Form, Col, Button, Row, Container } from 'react-bootstrap';
-import styles from '../../App.module.css';
+import formStyles from '../../styles/TaskCreateEditForm.css'
 import appStyles from "../../App.module.css";
 
 function TaskCreateForm() {
 
     /**Fetch task fields */
-    // const [taskData, setTaskData] = useState({
-    //     title: "",
-    //     description: "",
-    //     assigned_users: "",
-    //     overdue: "",
-    //     priority: "",
-    //     state: "",
-    //     attachment: "",
-    //     due_date: "",
-    // });
+    const [taskData, setTaskData] = useState({
+        title: "",
+        description: "",
+        assigned_users: "",
+        overdue: "",
+        priority: "",
+        state: "",
+        attachment: "",
+        due_date: "",
+    });
 
-    // const {title, description, assigned_users, overdue, priority, state, attachment, due_date} = taskData;
+    const {title, description, assigned_users, overdue, priority, state, attachment, due_date} = taskData;
+
+    /**Handles changes to the input fields */
+    const handleFormChange = (e) => {
+        setTaskData({
+            ...taskData,
+            [e.target.name]: e.target.value,
+        }) 
+    }
 
     const textFields = (
-        <div className='text-center'>
+        <div className={`${formStyles.div} text-center`}>
             <Form.Group >
                 <Form.Label>Title</Form.Label>
-                <Form.Control type="text" placeholder="task title" aria-label='title' />
+                <Form.Control name='title' aria-label='title' value={title} onChange={handleFormChange} />
             </Form.Group>
             <Form.Group >
                 <Form.Label>Task description</Form.Label>
-                <Form.Control as="textarea" aria-label='description' name='description' />
+                <Form.Control as="textarea" rows={6} aria-label='description' name='description' value={description} onChange={handleFormChange} />
             </Form.Group>
             <Form.Group >
                 <Form.Label>Assigned to</Form.Label>
-                <Form.Control as="select" aria-label='assigned_user' name='assigned_user' />
+                <Form.Control as="select" aria-label='assigned_user' name='assigned_user' value={assigned_users} onChange={handleFormChange} />
             </Form.Group>
             <Form.Group>
                 <Form.Label>Priority</Form.Label>
-                <Form.Select aria-label="priority">
+                <Form.Select aria-label="priority" onChange={handleFormChange} value={priority}>
                     <option value="1">Low</option>
                     <option value="2">Medium</option>
                     <option value="3">High</option>
@@ -43,7 +51,7 @@ function TaskCreateForm() {
             </Form.Group>
             <Form.Group>
                 <Form.Label>State</Form.Label>
-                <Form.Select aria-label="sate">
+                <Form.Select aria-label="sate" onChange={handleFormChange} value={state}>
                     <option value="1">Not started</option>
                     <option value="2">To-do</option>
                     <option value="3">In-progress</option>
@@ -57,6 +65,8 @@ function TaskCreateForm() {
                     name="due_date"
                     type="date"
                     aria-label="due_date"
+                    onChange={handleFormChange}
+                    value={due_date}
                 ></Form.Control>
             </Form.Group>
             <Form.Group className="position-relative mb-3">
@@ -65,11 +75,13 @@ function TaskCreateForm() {
                     type="file"
                     required
                     name="attachment"
+                    onChange={handleFormChange}
+                    value={attachment}
                 />
             </Form.Group>
             <Form.Group >
                 <Form.Label>Overdue</Form.Label>
-                <Form.Check type='switch' aria-label='overdue' name='overdue' />
+                <Form.Check type='switch' aria-label='overdue' name='overdue' value={overdue} onChange={handleFormChange} />
             </Form.Group>
             <Button type="submit" value="Submit">Create</Button>
             <Button>Cancel</Button>
@@ -79,15 +91,15 @@ function TaskCreateForm() {
 
     /**returns the task create form */
     return (
-        <Form className={styles.Form} >
+        <Form className={formStyles.Form} >
             <Row>
-                {/* <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+                <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
                     <Container
-                        className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+                        className={`${appStyles.Content} ${formStyles.Container} d-flex flex-column justify-content-center`}
                     >
                         <div className="d-md-none">{textFields}</div>
                     </Container>
-                </Col> */}
+                </Col>
                 <Col md={5} lg={4} className=" d-md-block p-0 p-md-2">
                     <Container className={appStyles.Content}>{textFields}</Container>
                 </Col>
