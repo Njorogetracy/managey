217f2c61dcb948858faa0d606d084d7ed92b6f57
Hope Njoroge <hnjoroge64@gmail.com>
2024-06-21 00:23:54 +0000

    deploy project

--- src/index.js
+++ src/index.js
@@ -10,7 +10,6 @@ import { ProfileDataProvider } from './contexts/ProfileDataContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
     <Router>
       <CurrentUserProvider>
         <ProfileDataProvider>
@@ -18,7 +17,6 @@ root.render(
         </ProfileDataProvider>
       </CurrentUserProvider>
     </Router>
-  </React.StrictMode>
 );
 
 export default App
