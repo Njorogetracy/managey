c8d7a06f6e057f96fd63fb6fe2a5ce9ef719efc5
Hope Tracy <hnjoroge64@gmail.com>
2024-12-26 10:49:44 +0000

    update api endpoint url

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://manageydrf-8a469d59154b.herokuapp.com/'
-axios.defaults.baseURL = 'https://8000-njorogetracy-manageydrf-fouja6zojup.ws-eu117.gitpod.io/'
+axios.defaults.baseURL = 'https://3000-njorogetracy-managey-hppr9jkculs.ws-eu117.gitpod.io/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
 
