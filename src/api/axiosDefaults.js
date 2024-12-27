be93d6db92cf7f1345f03955216b94b740a743b6
Hope Tracy <hnjoroge64@gmail.com>
2024-12-27 08:23:49 +0000

    update baseurl

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 axios.defaults.baseURL = 'https://manageydrf-8a469d59154b.herokuapp.com/'
+axios.defaults.baseURL = 'https://8000-njorogetracy-manageydrf-qt48gzd5j16.ws-eu117.gitpod.io/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true;
 
