7dd30773cac530b97d0112e0f8b076982d294993
Hope Tracy <hnjoroge64@gmail.com>
2025-01-05 16:00:30 +0000

    update api call

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-
+// Base Axios configurations
 axios.defaults.baseURL = 'https://manageydrf-8a469d59154b.herokuapp.com/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
@@ -15,21 +15,42 @@ export const axiosRes = axios.create({
   withCredentials: true,
 });
 
+// Function to get CSRF token from cookies
+const getCSRFToken = () => {
+  const csrfCookie = document.cookie.split('; ').find((row) => row.startsWith('csrftoken'));
+  return csrfCookie ? csrfCookie.split('=')[1] : null;
+};
+
+// Interceptor to include CSRF token in requests
 axiosReq.interceptors.request.use((config) => {
-  const token = localStorage.getItem('authToken');
-  if (token) {
-    config.headers['Authorization'] = `Token ${token}`;
+  const csrfToken = getCSRFToken();
+  const authToken = localStorage.getItem('authToken');
+
+  if (authToken) {
+    config.headers['Authorization'] = `Token ${authToken}`;
   }
+
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken;
+  }
+
   return config;
 }, (error) => {
   return Promise.reject(error);
 });
 
 axiosRes.interceptors.request.use((config) => {
-  const token = localStorage.getItem('authToken');
-  if (token) {
-    config.headers['Authorization'] = `Token ${token}`;
+  const csrfToken = getCSRFToken();
+  const authToken = localStorage.getItem('authToken'); 
+
+  if (authToken) {
+    config.headers['Authorization'] = `Token ${authToken}`;
   }
+
+  if (csrfToken) {
+    config.headers['X-CSRFToken'] = csrfToken;
+  }
+
   return config;
 }, (error) => {
   return Promise.reject(error);
