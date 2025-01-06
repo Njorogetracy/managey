9097e1ab8675972cf200e055329121744cf864ae
Hope Tracy <hnjoroge64@gmail.com>
2025-01-06 08:20:59 +0000

    add csrf token to the api call

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,8 +1,34 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://manageydrf-8a469d59154b.herokuapp.com/';
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
+axios.defaults.baseURL = "https://manageydrf-8a469d59154b.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+// Function to get CSRF token from cookies
+const getCsrfToken = () => {
+  const csrfToken = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("csrftoken="));
+  return csrfToken ? csrfToken.split("=")[1] : null;
+};
 
 export const axiosReq = axios.create();
 export const axiosRes = axios.create();
+
+// Add request interceptors to include `X-CSRFToken` and `Authorization` in headers
+axiosReq.interceptors.request.use(
+  (config) => {
+    const csrfToken = getCsrfToken();
+    const authToken = localStorage.getItem("authToken");
+
+    if (csrfToken) {
+      config.headers["X-CSRFToken"] = csrfToken;
+    }
+    if (authToken) {
+      config.headers["Authorization"] = `Token ${authToken}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
